8488dcf813b2d33b24e8751d70c828a4ac126bc4
koome <koomemc@gmail.com>
2024-03-25 21:19:06 +0300

    log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,35 @@
+import sys
+import re
+
+
+def format_each_line():
+    pattern = re.compile(r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\] "([^"]+)" (\d{3}) (\d+)$')
+    dictionary = {200: 0, 301: 0, 400: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    total_size = 0
+    count = 0
+    for line in sys.stdin:
+        text = pattern.search(line)
+        status_code = text[-2]
+        file_size = text[-1]
+        total_size += file_size
+        for key, value in dictionary.items():
+            if key == status_code:
+                value += 1
+        count += 1
+        if count == 10:
+            print(f"File size: {total_size}")
+            for key, value in dictionary.items():
+                print(f"{key}: {value}")
+            return format_each_line()
+                
+
+
+
+
+
+
+    
+
+
+
+
