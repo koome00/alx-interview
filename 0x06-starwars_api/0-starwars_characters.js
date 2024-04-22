dbcd39ea453df808345b16b88a370a18cf0a489f
koome <koomemc@gmail.com>
2024-04-21 17:15:30 -0700

    star wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,4 +1,18 @@
-#!/usr/bin/env node
-for (let j = 0; j < process.argv.length; j++) {
-    console.log(j + ' -> ' + (process.argv[j]));
-};
+#!/usr/bin/node
+const request = require('request');
+const filmNum = process.argv[2] + '/';
+const filmURL = 'https://swapi-api.alx-tools.com/';
+request(filmURL + filmNum, async (err, res, body) => {
+  if (err) return console.error(err);
+  const charURLList = JSON.parse(body).characters;
+  for (const charURL of charURLList) {
+    await new Promise((resolve, reject) => {
+      request(charURL, (err, res, body) => {
+        if (err) return console.error(err);
+
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
