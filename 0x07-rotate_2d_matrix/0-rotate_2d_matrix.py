d563ae8a868e50547399f6728ce0abd8d7613866
koome <koomemc@gmail.com>
2024-04-30 17:18:14 +0300

    rotate 2-d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def rotate_2d_matrix(matrix):
+    """
+    rotate matrix
+    """
+    new = []
+    for _ in range(len(matrix)):
+        new.append([0] * len(matrix[0]))
+    matrix.reverse()
+    count = 0
+    for row in matrix:
+        for col in range(len(row)):
+            print(col, count)
+            new[col][count] = matrix[count][col]
+        count += 1
+    matrix[:] = new
