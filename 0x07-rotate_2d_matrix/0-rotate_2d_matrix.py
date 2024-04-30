b79af19be3bba3b8dfbf901656fb4e00cc93ec3a
koome <koomemc@gmail.com>
2024-04-30 17:49:22 +0300

    in place rotation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,13 +6,13 @@ def rotate_2d_matrix(matrix: list[int]) -> list[int]:
     """
     rotate matrix
     """
-    new = []
-    for _ in range(len(matrix)):
-        new.append([0] * len(matrix[0]))
-    matrix.reverse()
-    count = 0
-    for row in matrix:
-        for col in range(len(row)):
-            new[col][count] = matrix[count][col]
-        count += 1
-    matrix[:] = new
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+    new_matrix = [[0] * rows for _ in range(cols)]
+
+    for i in range(rows):
+        for j in range(cols):
+            new_matrix[j][rows - i - 1] = matrix[i][j]
+
+    matrix[:] = new_matrix
