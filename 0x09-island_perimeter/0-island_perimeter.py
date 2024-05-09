87bed2bac0ae836a15f56ce5fe47f26c6cefb4ad
koome <koomemc@gmail.com>
2024-05-09 23:23:59 +0300

    account for outer indeces

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,22 +9,28 @@ def island_perimeter(grid):
     returns perimeter of a wall
     """
     total_peri = 0
+    assert (len(grid) <= 100)
     for x, row in enumerate(grid):
+        assert (len(row) <= 100)
         for y, element in enumerate(row):
             if element == 1:
                 peri = 4
                 # check bottom upto second last row
-                if grid[x+1][y] == 1 and x < (len(grid)-1):
-                    peri = peri - 1
+                if x < (len(grid) - 1):
+                    if grid[x+1][y] == 1:
+                        peri = peri - 1
                 # check right upto second last column
-                if grid[x][y+1] == 1 and y < (len(grid)-1):
-                    peri = peri - 1
+                if y < (len(row) - 1):
+                    if grid[x][y+1] == 1:
+                        peri = peri - 1
                 # check top upto second last row
-                if grid[x-1][y] == 1 and x > 0:
-                    peri = peri - 1
+                if x > 0:
+                    if grid[x-1][y] == 1:
+                        peri = peri - 1
                 # check left upto second last column
-                if grid[x][y-1] == 1 and y > 0:
-                    peri = peri - 1
+                if y > 0:
+                    if grid[x][y-1] == 1:
+                        peri = peri - 1
                 if peri == 4:
                     peri = 0
                 total_peri = peri + total_peri
