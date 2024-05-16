72c652ba645ae0a353f79a22222c6f159668e905
koome <koomemc@gmail.com>
2024-05-16 21:44:57 +0300

    check for each round and optimal play is chossing the max number

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -10,27 +10,45 @@ def isWinner(x, nums):
     Ben & Maria
     """
     wins = [0, 0]
-    maria = ben = 0
-    num = max(nums)
-    play = True
-    prime = [True for i in range(num+1)]
-    p = 2
-    while (p * p) <= num:
-        if prime[p] == True:
-            for i in range(p * p, num + 1, p):
-                prime[i] = False
-        p += 1
-    prime[0] = prime[1] = False
-    for value in prime:
-        if play and value:
-            maria += 1
-        if value and not play:
-            ben += 1
-        play = not play
-    if maria > ben:
-        wins[0] += 1
-    if maria < ben:
-        wins[1] += 1
+    sorted_primes = sorted(nums, reverse=True)
+    print(sorted_primes)
+    for num in sorted_primes:
+        if num == 1:
+            wins[1] += 1
+            continue
+        play = True
+        maria = ben = 0
+        prime = [True for i in range(num+1)]
+        p = 2
+        while (p * p) <= num:
+            if prime[p]:
+                for i in range(p * p, num + 1, p):
+                    prime[i] = False
+            p += 1
+        prime[0] = prime[1] = False
+        for value in prime:
+            if play and value:
+                maria += 1
+                play = not play
+                continue
+            if value and not play:
+                ben += 1
+                play = not play
+                continue
+        print(prime)
+        if maria > ben:
+            wins[0] += 1
+        if maria < ben:
+            wins[1] += 1
+        if maria == ben and maria % 2 == 0:
+            wins[0] += 1
+        if maria == ben and maria % 2 == 1:
+            wins[1] += 1
+        print(num, maria, ben)
+        x -= 1
+        if x == 0:
+            break
+
     if wins[0] > wins[1]:
         return "Maria"
     elif wins[1] > wins[0]:
